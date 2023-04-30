ac4b51c61550afbe517297fadb931590cf773735
unknown <ewarituy@gmail.com>
2023-04-30 12:31:23 +0300

    update

--- Assets/Scripts/Signaling.cs
+++ Assets/Scripts/Signaling.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -7,31 +8,42 @@ public class Signaling : MonoBehaviour
 
     private AudioSource _signalSource;
     private int _desiredVolume;
+    private Coroutine _currentCoroutine;
 
     private void Awake()
     {
         _signalSource = GetComponent<AudioSource>();
     }
 
-    private void Update()
-    {
-        if (_signalSource.volume == _desiredVolume)
-            return;
-
-        _signalSource.volume = Mathf.MoveTowards(_signalSource.volume, _desiredVolume, _volumeChangeRate * Time.deltaTime);
-
-        if (_signalSource.volume <= 0)
-            _signalSource.Stop();
-    }
-
     public void StartSignal()
     {
-        _signalSource.Play();
         _desiredVolume = 1;
+
+        StartChangeVolume();
     }
 
     public void StopSignal()
     {
         _desiredVolume = 0;
+
+        StartChangeVolume();
+    }
+
+    private void StartChangeVolume()
+    {
+        if (_currentCoroutine != null)
+            StopCoroutine(_currentCoroutine);
+
+        _currentCoroutine = StartCoroutine(ChangeVolume());
+    }
+
+    private IEnumerator ChangeVolume()
+    {
+        while(_signalSource.volume != _desiredVolume)
+        {
+            _signalSource.volume = Mathf.MoveTowards(_signalSource.volume, _desiredVolume, _volumeChangeRate * Time.deltaTime);
+
+            yield return null;
+        }
     }
 }
